48e82397d8bd54c0985322b8ab1c6224956af758
papa <kirya1455@mai.ru>
2020-02-22 14:21:32 +0300

    first commit

---
+++ vsearch.py
@@ -0,0 +1,5 @@
+def search4vowels(word:str) -> set:
+    vowels = set('aeiou')
+    return vowels.intersection(set(word))
+def search4letters(word:str, letters:str='aeiou') -> set:
+    return set(letters).intersection(set(word))
